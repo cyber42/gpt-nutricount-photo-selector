d66519d556f7c52171eba4e16d148045d84a753a
Mark Horvath <cyber42@gmail.com>
2024-01-26 18:50:55 +0100

    Logging

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def main(input_folder, target_folder, date, prompt_file):
     # Find photos
     photo_finder = PhotoFinder(input_folder, date)
     photos = photo_finder.find_photos()
+    print(f'All photos found on day {date}: {photos}')
 
     # Process images
     image_processor = ImageProcessor(target_folder, date)
@@ -28,5 +29,10 @@ if __name__ == "__main__":
     parser.add_argument("--date", help="The date for which to find photos (YYYY-MM-DD). Defaults to today's date.", default=datetime.now().strftime("%Y-%m-%d"))
     parser.add_argument("--prompt_file", help="The file containing the prompt for OpenAI API", default="nutricount-photo-select-prompt.txt")
     args = parser.parse_args()
+
+    print(f'Input folder: {args.input_folder}')
+    print(f'Target folder: {args.target_folder}')
+    print(f'Date: {args.date}')
+    print(f'Prompt file: {args.prompt_file}')
 
     main(args.input_folder, args.target_folder, args.date, args.prompt_file)
